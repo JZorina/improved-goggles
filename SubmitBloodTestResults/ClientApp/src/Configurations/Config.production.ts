9dfb1cd9ecad677b7a6c783fd523412050a8f411
yuli <yulia.z@zigit.co.il>
2022-03-17 01:43:37 +0200

    initial commit

--- SubmitBloodTestResults/ClientApp/src/Configurations/Config.production.ts
+++ SubmitBloodTestResults/ClientApp/src/Configurations/Config.production.ts
@@ -5,6 +5,6 @@ export const config: ConfigModal = {
     api: 'http://192.168.1.166:44351',
     useMockData: true,
     paths: {
-        getTestResult: '/api/TestResults/get-blood-tests'
+        getTestResult: '/api/TestResults/get-blood-tests',
     }
 };
